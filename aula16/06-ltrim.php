b1746e4405db610deaad914ef35ab046ab92116f
Pablo Santos <opablosantos@gmail.com>
2020-12-29 21:43:01 -0300

    Testando a função ltrim

---
+++ aula16/06-ltrim.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="_css/estilo.css"/>
+  <meta charset="UTF-8"/>
+  <title>Curso de PHP - CursoemVideo.com</title>
+</head>
+<body>
+<div>
+    <?php
+        $nome = "   José da Silva   ";
+        echo (strlen($nome));
+        echo "<br/>";
+        $novo = ltrim($nome);
+        echo $novo;
+        echo "<br/>";
+        echo (strlen($novo));
+        echo "<br/>";
+    ?>
+</div>
+</body>
+</html>
+ 
