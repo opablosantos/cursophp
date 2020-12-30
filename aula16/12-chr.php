266c85c62db888bb3bc584d18e44039c3f411e57
Pablo Santos <opablosantos@gmail.com>
2020-12-29 21:45:33 -0300

    Testando a função chr

---
+++ aula16/12-chr.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="_css/estilo.css"/>
+  <meta charset="UTF-8"/>
+  <title>Curso de PHP - CursoemVideo.com</title>
+</head>
+<body>
+<div>
+    <?php
+        $letra = chr(67);
+        echo $letra;
+        echo "<br/>";
+        $letra = chr(99);
+        echo $letra;
+    ?>
+</div>
+</body>
+</html>
+ 
