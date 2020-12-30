72bfa8f0026fb658dc175f00103cf624e08561d7
Pablo Santos <opablosantos@gmail.com>
2020-12-29 21:41:34 -0300

    Testando a função wordwrap

---
+++ aula16/03-wordwrap.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="_css/estilo.css"/>
+  <meta charset="UTF-8"/>
+  <title>Curso de PHP - CursoemVideo.com</title>
+</head>
+<body>
+<div>
+    <?php
+        $t = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+        $r = wordwrap($t, 40, "<br/>\n", false);
+        echo$r;
+    ?>
+</div>
+</body>
+</html>
+ 
